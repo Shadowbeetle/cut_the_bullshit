710ee05eb656db1c5ffbb8e2e3157d32c4971e76
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-04-26 15:00:13 +0200

    cascade deletes

--- priv/repo/migrations/20220426080652_create_posts.exs
+++ priv/repo/migrations/20220426080652_create_posts.exs
@@ -6,7 +6,7 @@ defmodule CutTheBullshit.Repo.Migrations.CreatePosts do
       add :title, :string, null: false
       add :description, :string, null: false
       add :url, :string, null: false
-      add :user_id, references(:users, on_delete: :nothing), null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: false
 
       timestamps()
     end
--- priv/repo/migrations/20220426085459_create_comments.exs
+++ priv/repo/migrations/20220426085459_create_comments.exs
@@ -4,8 +4,8 @@ defmodule CutTheBullshit.Repo.Migrations.CreateComments do
   def change do
     create table(:comments) do
       add :text, :string, null: false
-      add :user_id, references(:users, on_delete: :nothing), null: false
-      add :post_id, references(:posts, on_delete: :nothing), null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add :post_id, references(:posts, on_delete: :delete_all), null: false
 
       timestamps()
     end
