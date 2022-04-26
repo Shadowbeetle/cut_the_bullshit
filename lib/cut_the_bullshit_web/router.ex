42894eaba7b699a3eae31222e7796163efc0769d
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-04-26 15:31:01 +0200

    remove unnecessary files and routes

--- lib/cut_the_bullshit_web/router.ex
+++ lib/cut_the_bullshit_web/router.ex
@@ -71,17 +71,6 @@ defmodule CutTheBullshitWeb.Router do
     live "/posts/:id/comments/:comment_id/edit", PostLive.Show, :edit_comment
   end
 
-  scope "/", CutTheBullshitWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    live "/comments", CommentLive.Index, :index
-    live "/comments/new", CommentLive.Index, :new
-    live "/comments/:id/edit", CommentLive.Index, :edit
-
-    live "/comments/:id", CommentLive.Show, :show
-    live "/comments/:id/show/edit", CommentLive.Show, :edit
-  end
-
   ## Authentication routes
 
   scope "/", CutTheBullshitWeb do
