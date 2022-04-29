c754dbb7f39c2b14e5a526d283856e70670995f8
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-04-29 16:26:23 +0200

    color arrows based on user's vote

--- lib/cut_the_bullshit_web/live/post_live/vote_component.ex
+++ lib/cut_the_bullshit_web/live/post_live/vote_component.ex
@@ -13,15 +13,35 @@ defmodule CutTheBullshitWeb.PostLive.VoteComponent do
   end
 
   @impl true
-  def handle_event("upvote", _params, socket) do
-    case Posts.vote_on_post(socket.assigns.post, socket.assigns.current_user, :up) do
-      {:ok, %{post: post, vote: _}} ->
+  def handle_event("vote", %{"vote-type" => vote_type} = _params, socket) do
+    case Posts.vote_on_post(
+           socket.assigns.post,
+           socket.assigns.current_user,
+           vote_type |> String.to_atom()
+         ) do
+      {:ok, %{post: post, vote: vote}} ->
         {:noreply,
          socket
-         |> assign(post: post)}
+         |> assign(post: post |> Map.put(:vote_of_current_user, vote.value))}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+  defp get_up_arrow_style(vote_of_current_user) do
+    case vote_of_current_user do
+      :up -> "color: red"
+      :down -> ""
+      _ -> ""
+    end
+  end
+
+  defp get_down_arrow_style(vote_of_current_user) do
+    case vote_of_current_user do
+      :down -> "color: red"
+      :up -> ""
+      _ -> ""
     end
   end
 end
