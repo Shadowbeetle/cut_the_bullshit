fa2a3dbb66577675d367b8fef01fa7c3168c4f82
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-05-08 16:33:42 +0200

    roll url hash into view

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,8 +32,13 @@ let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToke
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+window.addEventListener("phx:page-loading-start", info => {
+	topbar.show()
+	document.querySelector(window.location.hash).scrollIntoView()
+})
+window.addEventListener("phx:page-loading-stop", info => {
+	topbar.hide()
+})
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -43,4 +48,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
